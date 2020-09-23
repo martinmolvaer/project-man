95f1a9e9dc9a59b6ec484f541ea7918e63b0bf67
Martin <martin.molvaer@gmail.com>
2020-09-23 13:09:59 +0200

    add router, home and dependencies
    this is getting litty

--- project-man/src/App.js
+++ project-man/src/App.js
@@ -1,24 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Home from "./components/Home";
+import Projects from "./components/Projects";
+import { BrowserRouter, Switch, Route, Link } from "react-router-dom";
+import { Container } from "react-bootstrap";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Container>
+        <BrowserRouter>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/projects">Projects</Link>
+            </li>
+          </ul>
+          <Switch>
+            <Route exact path="/" component={Home}></Route>
+            <Route exact path="/projects" component={Projects}></Route>
+          </Switch>
+        </BrowserRouter>
+      </Container>
     </div>
   );
 }
---
+++ project-man/src/components/Projects.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Projects = () => {
+  return (
+    <div>
+      <h1>Hei 2</h1>
+    </div>
+  );
+};
+
+export default Projects;
