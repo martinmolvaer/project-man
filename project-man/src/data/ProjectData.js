d5cec5a7dc8f30bdf1e6b5af4a2c3a5c37d06faf
Martin <martin.molvaer@gmail.com>
2020-09-23 14:01:27 +0200

    add dummy data

---
+++ project-man/src/data/ProjectData.js
@@ -0,0 +1,87 @@
+[
+  {
+    id: 1,
+    name: "Span",
+    status: null,
+    description:
+      "Morbi sem mauris, laoreet ut, rhoncus aliquet, pulvinar sed, nisl.",
+    customerId: 1,
+    employeeId: 1,
+  },
+  {
+    id: 2,
+    name: "Regrant",
+    status: null,
+    description: "Sed sagittis.",
+    customerId: 2,
+    employeeId: 2,
+  },
+  {
+    id: 3,
+    name: "Latlux",
+    status: null,
+    description:
+      "Morbi vestibulum, velit id pretium iaculis, diam erat fermentum justo, nec condimentum neque sapien placerat ante. Nulla justo.",
+    customerId: 3,
+    employeeId: 3,
+  },
+  {
+    id: 4,
+    name: "Tin",
+    status: null,
+    description:
+      "Maecenas tincidunt lacus at velit. Vivamus vel nulla eget eros elementum pellentesque.",
+    customerId: 4,
+    employeeId: 4,
+  },
+  {
+    id: 5,
+    name: "Andalax",
+    status: null,
+    description: "In quis justo. Maecenas rhoncus aliquam lacus.",
+    customerId: 5,
+    employeeId: 5,
+  },
+  {
+    id: 6,
+    name: "Overhold",
+    status: null,
+    description: "Aliquam erat volutpat. In congue.",
+    customerId: 6,
+    employeeId: 6,
+  },
+  {
+    id: 7,
+    name: "Treeflex",
+    status: null,
+    description:
+      "Morbi sem mauris, laoreet ut, rhoncus aliquet, pulvinar sed, nisl. Nunc rhoncus dui vel sem.",
+    customerId: 7,
+    employeeId: 7,
+  },
+  {
+    id: 8,
+    name: "Vagram",
+    status: null,
+    description:
+      "In blandit ultrices enim. Lorem ipsum dolor sit amet, consectetuer adipiscing elit.",
+    customerId: 8,
+    employeeId: 8,
+  },
+  {
+    id: 9,
+    name: "Home Ing",
+    status: null,
+    description: "Duis at velit eu est congue elementum.",
+    customerId: 9,
+    employeeId: 9,
+  },
+  {
+    id: 10,
+    name: "Quo Lux",
+    status: null,
+    description: "Quisque ut erat. Curabitur gravida nisi at nibh.",
+    customerId: 10,
+    employeeId: 10,
+  },
+];
